a73cbab1dbfb49a6b34ee72ade01add8ecee3fc6
lucas-m-d <68205310+lucas-m-d@users.noreply.github.com>
2021-03-30 20:06:09 +0100

    changes to sketch.js

--- sketch.js
+++ sketch.js
@@ -10,22 +10,20 @@ const elasticCollision1D = (m1, m2, v1, v2) => {
 }
 const collision2d = ( m1, m2, p1, p2, v1, v2) => {
   const normal = p5.Vector.sub(p2, p1)  
-  //const unitNormal = normal.copy().div(p5.Vector.mag(normal))
   const unitNormal = p5.Vector.div(normal, normal.mag())
   const unitTan = createVector(-unitNormal.copy().y, unitNormal.copy().x) 
   let v1n = p5.Vector.dot(unitNormal, v1)
-  let v1t = p5.Vector.dot(unitTan, v1) // v1t = v1tp && v2t = v2tp
+  let v1t = p5.Vector.dot(unitTan, v1) 
   let v2n = p5.Vector.dot(unitNormal, v2)
   let v2t = p5.Vector.dot(unitTan, v2)
-  //let v1np = ((v1n * (m1 - m2)) + (2 * (m2 * v2n))) / (m1 + m2) //scalars
+  
   let normalVelocities = elasticCollision1D(m1, m2, v1n, v2n)
   let v1np = normalVelocities.v1
   let v2np = normalVelocities.v2
   let tangentVelocities = elasticCollision1D(m1, m2, v1t, v2t)
   let v1tp = tangentVelocities.v1
   let v2tp = tangentVelocities.v2
-  //let v1tp = v1t
-  //let v2tp = v2t
+
   let vec1np = p5.Vector.mult(unitNormal, v1np) //vectors
   let vec1tp = p5.Vector.mult(unitTan, v1tp)
   let vec2np = p5.Vector.mult(unitNormal, v2np)
@@ -34,10 +32,14 @@ const collision2d = ( m1, m2, p1, p2, v1, v2) => {
   var finalv2 = p5.Vector.add(vec2np, vec2tp)//vec2np.copy().add(vec2tp)
   return ({ "v1" : finalv1, "v2" : finalv2}) 
 }
-const momentumBefore = () => {return(object1.velocity.copy().mult(object1.mass).add(object2.velocity.copy().mult(object2.mass)))}
+const momentum = () => {
+  let momentum1 = p5.Vector.mult(object1.velocity, object1.size)
+  let momentum2 = p5.Vector.mult(object2.velocity, object2.size)
+  return (momentum1.add(momentum2).mag())
+}
 const kineticEnergy = () => { 
-  var ke1 = 0.5 * object1.size * object1.velocity.x * object1.velocity.x  
-  var ke2 = 0.5 * object2.size * object2.velocity.x * object2.velocity.x
+  var ke1 = 0.5 * object1.size * object1.velocity.mag() * object1.velocity.mag()
+  var ke2 = 0.5 * object2.size * object2.velocity.mag() * object2.velocity.mag()
   return (ke1 + ke2)
 }
 const collide = () => {
@@ -55,20 +57,20 @@ function setup() {
 }
 
 function draw() {
-  background(200)
+  background(0)
+  noStroke()
   object1.update()
   object2.update()
   if (object1.vector.x <= object2.vector.x + object2.size && object1.vector.x >= object2.vector.x && (object1.vector.y >= object2.vector.y && object1.vector.y <= object2.vector.y + object2.size )){
-    p1 = momentumBefore()
-    p1add = p1.x + p1.y
+    p1 = momentum()
     collide()
-    p2 = momentumBefore()
-    p2add = p2.x + p2.y
-    console.log("momentum before = "+ p1add + "momentum after = " + p2add )
+    p2 = momentum()
+    console.log("momentum before = "+ p1 + "momentum after = " + p2)
     object1.update()
   object2.update()
   }  
   object2.display()
   object1.display()
-  text("object1 velocity = " + object1.velocity.x + "\nobject2 velocity = " + object2.velocity.x + "\nMomentum X, Y = " + momentumBefore().x + ", " + momentumBefore().y + "\nKE = " + kineticEnergy(), 0, 300 ) 
+  //text("object1 velocity = " + object1.velocity.x + "\nobject2 velocity = " + object2.velocity.x + "\nMomentum X, Y = " + momentumBefore().x + ", " + momentumBefore().y + "\nKE = " + kineticEnergy(), 0, 300 ) 
+  document.getElementById("info").innerHTML = "<p> Object 1 velocity = " + object1.velocity.mag() + "</p> <p> Momentum = " + momentum() + "</p><p>KE = " + kineticEnergy() + "</p>"
 }
