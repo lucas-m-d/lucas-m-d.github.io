9fa5f7d5668cd8e0586a249213de1206e3fffe68
lucas-m-d <68205310+lucas-m-d@users.noreply.github.com>
2021-05-06 14:13:58 +0100

    Major Update
    Added arrows
    "Fling" the balls with right click
    Larger canvas

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
-const object1 = new SquareObject()
-const object2 = new SquareObject()
+const object1 = new CircleObject()
+const object2 = new CircleObject()
 // NOTE - THERE IS A DIFFERENT EQUATION FOR 2D MOMENTUM
 // https://imada.sdu.dk/~rolf/Edu/DM815/E10/2dcollisions.pdf
+
+const screen = {
+    "x":600,
+    "y":600
+}
 
 const circleCollide = (x1, x2, y1, y2, r1, r2) => {
   var dist = Math.sqrt((x2 - x1 ) ** 2 + (y2 - y1) ** 2);
@@ -15,6 +20,7 @@ const elasticCollision1D = (m1, m2, v1, v2) => {
   var object1speed = ((m1-m2)/(m1 + m2)) * v1 + ((2 * m2)/(m1 + m2)) * v2 // http://hyperphysics.phy-astr.gsu.edu/hbase/elacol2.html#c3  
   return ({"v1":object1speed, "v2":object2speed})
 }
+
 const collision2d = ( m1, m2, p1, p2, v1, v2) => {
   const normal = p5.Vector.sub(p2, p1)  
   const unitNormal = p5.Vector.div(normal, normal.mag())
@@ -36,7 +42,7 @@ const collision2d = ( m1, m2, p1, p2, v1, v2) => {
   let vec2np = p5.Vector.mult(unitNormal, v2np)
   let vec2tp = p5.Vector.mult(unitTan, v2tp)
   var finalv1 = p5.Vector.add(vec1np, vec1tp)
-  var finalv2 = p5.Vector.add(vec2np, vec2tp)//vec2np.copy().add(vec2tp)
+  var finalv2 = p5.Vector.add(vec2np, vec2tp)
   return ({ "v1" : finalv1, "v2" : finalv2}) 
 }
 const momentum = () => {
@@ -55,30 +61,59 @@ const collide = () => {
   object2.velocity.set(velocities.v2, object2)
 }
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(screen.x, screen.y);
   textSize(16);
   object1.init(0, 0, 4)
-  object1.vector.x = Math.round(random(object1.size, 400 - object1.size))
-  object1.vector.y = Math.round(random(object1.size, 400 - object1.size))
+  object1.vector.x = Math.round(random(object1.size, screen.x - object1.size))
+  object1.vector.y = Math.round(random(object1.size, screen.y - object1.size))
   object2.init(0, 0, 64)
-  object2.vector.x = Math.round(random(object2.size, 400 - object2.size))
-  object2.vector.y = Math.round(random(object2.size, 400 - object2.size))
+  object2.vector.x = Math.round(random(object2.size, screen.x - object2.size))
+  object2.vector.y = Math.round(random(object2.size, screen.y - object2.size))
+  
   
 }
+var mouseSpeed;
+obj2update = true
+obj1update = true
+obj2drag = false
+obj1drag = false
 
+for (let element of document.getElementsByClassName("p5Canvas")) { // https://stackoverflow.com/questions/60853612/p5-js-on-right-mouse-click-shows-the-browser-context-menu-and-not-the-drawing-fu
+    element.addEventListener("contextmenu", (e) => e.preventDefault());
+  }
 function mouseDragged(){
-  var dx = (object2.vector.x - mouseX) ** 2
-  var dy = (object2.vector.y - mouseY) ** 2
-  var dist = Math.sqrt(dx + dy)
-  if (dist <= object2.size){
-    object2.vector = createVector(mouseX, mouseY)
+  
+  var dx2 = (object2.vector.x - mouseX) ** 2
+  var dy2 = (object2.vector.y - mouseY) ** 2
+  var dist2 = Math.sqrt(dx2 + dy2)
+  if (dist2 <= object2.size){
+    if (obj1drag === false){
+      obj2drag = true
+      //object2.velocity = mouseSpeed // drag mouse velocity
+      object2.vector = createVector(mouseX, mouseY)
+      obj2update = false 
+    }
   }
-  dx = (object1.vector.x - mouseX) ** 2
-  dy = (object1.vector.y - mouseY) ** 2
-  dist = Math.sqrt(dx + dy)
-  if (dist <= (object1.size + 10)){
-    object1.vector = createVector(mouseX, mouseY)
+  
+    var dx1 = (object1.vector.x - mouseX) ** 2
+    var dy1 = (object1.vector.y - mouseY) ** 2
+    var dist1 = Math.sqrt(dx1 + dy1)
+    if (dist1 <= (object1.size + 10)){
+      if (obj2drag === false){
+        obj1drag = true
+        object1.vector = createVector(mouseX, mouseY)
+        //object1.velocity = mouseSpeed
+        obj1update = false
+    }
   }
+  if (mouseButton === RIGHT){if(obj1drag){object1.velocity = createVector(movedX, movedY)};if(obj2drag){object2.velocity = createVector(movedX, movedY)}
+  }
+}
+function mouseReleased(){
+  obj2update = true
+  obj1update = true
+  obj2drag = false
+  obj1drag = false
 }
 
 
@@ -96,15 +131,15 @@ function draw() {
     object2.size = parseInt(document.getElementById("size2").value)
     console.log(size2Div.value)
   }
+  if (obj1update){object1.update()}
   
-  object1.update()
-  object2.update()
+  if(obj2update){object2.update()}
   if (circleCollide(object1.vector.x, object2.vector.x, object1.vector.y, object2.vector.y, object1.size, object2.size)){
     console.log("hit")
     p1 = momentum()
     collide()
     p2 = momentum()
-    console.log("momentum before = "+ p1 + "momentum after = " + p2)
+    console.log("momentum before = "+ p1 + " momentum after = " + p2)
   }
  
   object2.display()
--- square.js
+++ square.js
@@ -1,16 +1,30 @@
-const screen = {
-    "x":400,
-    "y":400
+function drawArrow(base, vec, myColor) { // from p5 docs
+  push();
+  stroke(myColor);
+  strokeWeight(1);
+  fill(myColor);
+  translate(base.x, base.y);
+  line(0, 0, vec.x, vec.y);
+  rotate(vec.heading());
+  let arrowSize = 7;
+  translate(vec.mag() - arrowSize, 0);
+  triangle(0, arrowSize / 3, 0, -arrowSize / 3, arrowSize, 0);
+  pop();
+}
+var arrow = true
+
+function changeArrow(){
+  
+  if (arrow){arrow = false} else { arrow = true}
+  console.log("arrow changed")
 }
 
-class SquareObject {
+class CircleObject {
    constructor(){
        this.vector;
        this.velocity;
        //this.momentum;
-       this.size;
-       
-       
+       this.size;     
    }
    init (x, y, size){
     this.vector = createVector(x, y)
@@ -20,12 +34,26 @@ class SquareObject {
    }
    display(){
        circle(this.vector.x, this.vector.y, this.size * 2)
-   }
-   update (collision=false) {
-       if (this.vector.x - this.size <= 0 || this.vector.x + this.size >= screen.x){
-           this.velocity.x *= -1
-       } else if (this.vector.y - this.size<=0 || this.vector.y + this.size >= screen.y){
-           this.velocity.y *= -1
+       if (arrow){
+       drawArrow(this.vector, p5.Vector.mult(this.velocity, 10), 'blue');
+       }
+    }
+   update () {
+       if (this.vector.x - this.size <= 0){
+           if (this.velocity.x <= 0){
+             this.velocity.x *= -1
+           }
+       }
+       if (this.vector.x + this.size >= screen.x){
+         if (this.velocity.x >= 0){
+             this.velocity.x *= -1
+           }
+       }
+       if (this.vector.y - this.size<=0){
+          if (this.velocity.y <= 0){this.velocity.y *= -1}
+       }
+       if (this.vector.y + this.size >= screen.y){
+         if (this.velocity.y >= 0){this.velocity.y *= -1}
        }
        this.vector.add(this.velocity)
        
